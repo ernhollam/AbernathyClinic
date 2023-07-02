e5691923da54649f6c056f3ca8a449eb45cd1d3c
ernhollam <ernholla.marinasy@gmail.com>
2023-07-02 02:20:26 +0200

    Add @EnableFeignClients annotation

--- clientui/src/main/java/com/abernathyclinic/clientui/ClientuiApplication.java
+++ clientui/src/main/java/com/abernathyclinic/clientui/ClientuiApplication.java
@@ -2,8 +2,10 @@ package com.abernathyclinic.clientui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.abernathyclinic")
 public class ClientuiApplication {
 
 	public static void main(String[] args) {
