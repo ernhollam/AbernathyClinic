e5ab43254baa31bdca83af58d885726d013b6d83
e.marinasy <ernholla.marinasy@gmail.com>
2023-07-11 15:48:45 +0200

    Create HistoryProxy

---
+++ clientui/src/main/java/com/abernathyclinic/clientui/proxy/HistoryProxy.java
@@ -0,0 +1,34 @@
+package com.abernathyclinic.clientui.proxy;
+
+import com.abernathyclinic.clientui.bean.NoteBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@FeignClient(name = "history", url = "${history.url}")
+public interface HistoryProxy {
+    @PostMapping("/patHistory")
+    NoteBean createNote(@Valid @RequestBody NoteBean patient);
+
+    @GetMapping("/patHistory")
+    List<NoteBean> getNotes();
+
+    @GetMapping("/patHistory/{id}")
+    NoteBean getNoteById(@PathVariable("id") Integer id);
+
+    @GetMapping("/patHistory/patient/{patientId}")
+    List<NoteBean> getPatientHistory(@PathVariable("id") Integer patientId);
+
+    @PutMapping("/patHistory/{id}")
+    NoteBean updateNote(@PathVariable("id") Integer id, @Valid @RequestBody NoteBean patient);
+
+    @DeleteMapping("/patHistory/{id}")
+    void deleteNote(@PathVariable("id") Integer id);
+}
