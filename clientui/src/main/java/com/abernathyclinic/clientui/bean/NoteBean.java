9faf7613f367e9212601fd7e3e5013b20c7f5204
e.marinasy <ernholla.marinasy@gmail.com>
2023-07-11 15:44:49 +0200

    Create NoteBean

---
+++ clientui/src/main/java/com/abernathyclinic/clientui/bean/NoteBean.java
@@ -0,0 +1,29 @@
+package com.abernathyclinic.clientui.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class NoteBean {
+	private Integer   id;
+	@NotNull(message = "You must provide a patient ID")
+	Integer patId;
+	@NotBlank(message = "Notes/recommendations can not be blank")
+	String content;
+
+	@Override
+	public String toString() {
+		return "{\n" +
+				"id = " + id + ",\n "+
+				"patId = \"" + patId + "\",\n "+
+				"content =\"" + content + "\",\n "+"}";
+	}
+}
