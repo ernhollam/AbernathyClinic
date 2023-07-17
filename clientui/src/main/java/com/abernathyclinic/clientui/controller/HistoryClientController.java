1a097acee97810e59e2144aba4d238595affad0a
ernhollam <ernholla.marinasy@gmail.com>
2023-07-17 14:39:41 +0200

    Add model attribute to detect current page

--- clientui/src/main/java/com/abernathyclinic/clientui/controller/HistoryClientController.java
+++ clientui/src/main/java/com/abernathyclinic/clientui/controller/HistoryClientController.java
@@ -21,180 +21,193 @@ import javax.validation.Valid;
 @Controller
 @Slf4j
 public class HistoryClientController {
-    @Autowired
-    private PatientProxy patientProxy;
-    @Autowired
-    private HistoryProxy historyProxy;
+	@Autowired
+	private PatientProxy patientProxy;
+	@Autowired
+	private HistoryProxy historyProxy;
 
+	/**
+	 * Show list of notes for patient if they exist
+	 *
+	 * @param model holder for context data to be passed from controller to the view, contains list of notes
+	 * @return Patient history, list of patients if patient was not found
+	 */
+	@GetMapping("/patHistory/{patientId}")
+	public String showPatientHistory(@PathVariable("patientId") Integer patientId, Model model,
+			RedirectAttributes redirectAttributes) {
+		PatientBean patient = getPatientIfExists(patientId, redirectAttributes);
+		if (patient == null) {
+			return "patient/list";
+		}
+		model.addAttribute("patHistory", historyProxy.getPatientHistory(patientId));
+		model.addAttribute("patient", patient);
+		model.addAttribute("currentPage", "history");
+		return "/note/list";
+	}
 
-    /**
-     * Show list of notes for patient if they exist
-     *
-     * @param model holder for context data to be passed from controller to the view, contains list of notes
-     * @return Patient history, list of patients if patient was not found
-     */
-    @GetMapping("/patHistory/{patientId}")
-    public String showPatientHistory(@PathVariable("patientId") Integer patientId, Model model, RedirectAttributes redirectAttributes) {
-        PatientBean patient = getPatientIfExists(patientId, redirectAttributes);
-        if (patient == null) {
-            return "patient/list";
-        }
-        model.addAttribute("patHistory", historyProxy.getPatientHistory(patientId));
-        model.addAttribute("patient", patient);
-        return "/note/list";
-    }
+	/**
+	 * Shows update a note form.
+	 *
+	 * @param noteId ID of note to update
+	 * @param model  holder for context data to be passed from controller to the view, contains note to be updated
+	 * @return update note page if error, list of notes otherwise
+	 */
+	@GetMapping("/patHistory/{patientId}/update/{noteId}")
+	public String showUpdateNoteForm(@PathVariable("patientId") Integer patientId,
+			@PathVariable("noteId") String noteId, Model model, RedirectAttributes redirectAttributes) {
+		PatientBean patient = getPatientIfExists(patientId, redirectAttributes);
+		if (patient == null) {
+			return "patient/list";
+		}
+		NoteBean note = historyProxy.getNoteById(noteId);
+		if (note != null) {
+			model.addAttribute("note", note);
+			model.addAttribute("patient", patient);
+			return "note/update";
+		}
+		// return to patient profile if note was not found
+		redirectAttributes.addFlashAttribute("error", "Note with ID " + noteId + " does not exist.");
+		model.addAttribute("patHistory", historyProxy.getPatientHistory(patientId));
+		model.addAttribute("currentPage", "history");
+		return "redirect:/note/list";
+	}
 
-    /**
-     * Shows update a note form.
-     *
-     * @param noteId ID of note to update
-     * @param model  holder for context data to be passed from controller to the view, contains note to be updated
-     * @return update note page if error, list of notes otherwise
-     */
-    @GetMapping("/patHistory/{patientId}/update/{noteId}")
-    public String showUpdateNoteForm(@PathVariable("patientId") Integer patientId, @PathVariable("noteId") String noteId, Model model, RedirectAttributes redirectAttributes) {
-        PatientBean patient = getPatientIfExists(patientId, redirectAttributes);
-        if (patient == null) {
-            return "patient/list";
-        }
-        NoteBean note = historyProxy.getNoteById(noteId);
-        if (note != null) {
-            model.addAttribute("note", note);
-            model.addAttribute("patient", patient);
-            return "note/update";
-        }
-        // return to patient profile if note was not found
-        redirectAttributes.addFlashAttribute("error", "Note with ID " + noteId + " does not exist.");
-        model.addAttribute("patHistory", historyProxy.getPatientHistory(patientId));
-        return "redirect:/note/list";
-    }
+	/**
+	 * Updates a note.
+	 *
+	 * @param noteId             ID of note to be updated
+	 * @param note               updated note
+	 * @param result             result of validation form
+	 * @param model              holder for context data to be passed from controller to the view, contains note to update and list of notes
+	 * @param redirectAttributes redirection attributes, contains success popup
+	 * @return list of notes if update is successful, update note page otherwise
+	 */
+	@PostMapping("/patHistory/{patientId}/update/{noteId}")
+	public String updateNote(@PathVariable("patientId") Integer patientId, @PathVariable("noteId") String noteId,
+			@Valid NoteBean note,
+			BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+		PatientBean patient = getPatientIfExists(patientId, redirectAttributes);
+		if (patient == null) {
+			return "patient/list";
+		}
+		// check required fields
+		if (result.hasErrors()) {
+			note.setId(noteId);
+			model.addAttribute("note", note);
+			return "note/update";
+		}
+		// if valid call service to update NoteBean
+		note.setId(noteId);
+		try {
+			historyProxy.updateNote(noteId, note);
+			redirectAttributes.addFlashAttribute("success", "Note " + note.getId() + " was successfully updated.");
+		} catch (Exception exception) {
+			redirectAttributes.addFlashAttribute("error",
+					"Error while trying to update note " + note.getId() + ":\n" + exception.getMessage());
+		}
+		model.addAttribute("patHistory", historyProxy.getPatientHistory(patientId));
+		model.addAttribute("patient", patient);
+		model.addAttribute("currentPage", "history");
+		return "redirect:/note/list";
+	}
 
-    /**
-     * Updates a note.
-     *
-     * @param noteId             ID of note to be updated
-     * @param note               updated note
-     * @param result             result of validation form
-     * @param model              holder for context data to be passed from controller to the view, contains note to update and list of notes
-     * @param redirectAttributes redirection attributes, contains success popup
-     * @return list of notes if update is successful, update note page otherwise
-     */
-    @PostMapping("/patHistory/{patientId}/update/{noteId}")
-    public String updateNote(@PathVariable("patientId") Integer patientId, @PathVariable("noteId") String noteId, @Valid NoteBean note,
-                             BindingResult result, Model model, RedirectAttributes redirectAttributes) {
-        PatientBean patient = getPatientIfExists(patientId, redirectAttributes);
-        if (patient == null) {
-            return "patient/list";
-        }
-        // check required fields
-        if (result.hasErrors()) {
-            note.setId(noteId);
-            model.addAttribute("note", note);
-            return "note/update";
-        }
-        // if valid call service to update NoteBean
-        note.setId(noteId);
-        try {
-            historyProxy.updateNote(noteId, note);
-            redirectAttributes.addFlashAttribute("success", "Note " + note.getId() + " was successfully updated.");
-        } catch (Exception exception) {
-            redirectAttributes.addFlashAttribute("error", "Error while trying to update note " + note.getId() + ":\n" + exception.getMessage());
-        }
-        model.addAttribute("patHistory", historyProxy.getPatientHistory(patientId));
-        model.addAttribute("patient", patient);
-        return "redirect:/note/list";
-    }
+	/**
+	 * Shows add note form. User should provide at least a family name, a given name and a date of birth
+	 *
+	 * @param note note to be added
+	 * @return add note page
+	 */
+	@GetMapping("/patHistory/{patientId}/add")
+	public String showAddNoteForm(@PathVariable("patientId") Integer patientId, NoteBean note, Model model) {
+		note.setPatId(patientId);
+		model.addAttribute("note", note);
+		model.addAttribute("patient", patientProxy.getPatientById(note.getPatId()));
+		model.addAttribute("currentPage", "history");
+		return "note/add";
+	}
 
-    /**
-     * Shows add note form. User should provide at least a family name, a given name and a date of birth
-     *
-     * @param note note to be added
-     * @return add note page
-     */
-    @GetMapping("/patHistory/{patientId}/add")
-    public String showAddNoteForm(@PathVariable("patientId") Integer patientId, NoteBean note, Model model) {
-        note.setPatId(patientId);
-        model.addAttribute("note", note);
-        model.addAttribute("patient", patientProxy.getPatientById(note.getPatId()));
-        return "note/add";
-    }
+	/**
+	 * Validates add note form and redirects to notes list if successful.
+	 *
+	 * @param note               note to be added
+	 * @param result             result of form validation
+	 * @param model              holder for context data to be passed from controller to the view, contains list of notes
+	 * @param redirectAttributes redirection attributes, contains success popup
+	 * @return add note page if an error occurred, list of notes otherwise
+	 */
+	@PostMapping("/patHistory/{patientId}/add")
+	public String addNoteForPatient(@PathVariable("patientId") Integer patientId, @Valid @RequestBody NoteBean note,
+			BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+		PatientBean patient = getPatientIfExists(patientId, redirectAttributes);
+		if (patient == null) {
+			return "patient/list";
+		}
+		model.addAttribute("patient", patient);
+		note.setPatId(patientId);
+		model.addAttribute("note", note);
+		if (!result.hasErrors()) {
+			try {
+				historyProxy.createNote(note);
+				redirectAttributes.addFlashAttribute("success",
+						"Note " + note.getContent() + " was successfully created.");
+				model.addAttribute("notes", patientProxy.getPatients());
+			} catch (Exception exception) {
+				redirectAttributes.addFlashAttribute("error",
+						"Error while trying to add note " + note.getContent() + ":\n" + exception.getMessage());
+			}
+			model.addAttribute("patHistory", historyProxy.getPatientHistory(patientId));
+			model.addAttribute("currentPage", "history");
+			// redirect to list of notes page
+			return "redirect:/note/list";
+		}
+		return "note/add";
+	}
 
-    /**
-     * Validates add note form and redirects to notes list if successful.
-     *
-     * @param note               note to be added
-     * @param result             result of form validation
-     * @param model              holder for context data to be passed from controller to the view, contains list of notes
-     * @param redirectAttributes redirection attributes, contains success popup
-     * @return add note page if an error occurred, list of notes otherwise
-     */
-    @PostMapping("/patHistory/{patientId}/add")
-    public String addNoteForPatient(@PathVariable("patientId") Integer patientId, @Valid @RequestBody NoteBean note, BindingResult result, Model model, RedirectAttributes redirectAttributes) {
-        PatientBean patient = getPatientIfExists(patientId, redirectAttributes);
-        if (patient == null) {
-            return "patient/list";
-        }
-        model.addAttribute("patient", patient);
-        note.setPatId(patientId);
-        model.addAttribute("note", note);
-        if (!result.hasErrors()) {
-            try {
-                historyProxy.createNote(note);
-                redirectAttributes.addFlashAttribute("success", "Note " + note.getContent() + " was successfully created.");
-                model.addAttribute("notes", patientProxy.getPatients());
-            } catch (Exception exception) {
-                redirectAttributes.addFlashAttribute("error", "Error while trying to add note " + note.getContent() + ":\n" + exception.getMessage());
-            }
-            model.addAttribute("patHistory", historyProxy.getPatientHistory(patientId));
-            // redirect to list of notes page
-            return "redirect:/note/list";
-        }
-        return "note/add";
-    }
+	/**
+	 * Deletes a note.
+	 *
+	 * @param patientId          ID of note to be deleted
+	 * @param model              holder for context data to be passed from controller to the view, contains list of notes
+	 * @param redirectAttributes redirection attributes, contains success or failure popup
+	 * @return list of notes page
+	 */
+	@GetMapping("/patHistory/{patientId}/delete/{noteId}")
+	public String deletePatient(@PathVariable("patientId") Integer patientId, @PathVariable("noteId") String noteId,
+			Model model, RedirectAttributes redirectAttributes) {
+		PatientBean patient = getPatientIfExists(patientId, redirectAttributes);
+		if (patient == null) {
+			return "patient/list";
+		}
+		// Find NoteBean by ID
+		NoteBean note = historyProxy.getNoteById(noteId);
+		// and delete the note
+		if (note != null) {
+			historyProxy.deleteNote(noteId);
+			redirectAttributes.addFlashAttribute("success", "Note " + note.getContent() + " was successfully deleted.");
+			model.addAttribute("patient", patient);
+		} else {
+			redirectAttributes.addFlashAttribute("error", "Note with ID " + noteId + " does not exist.");
+		}
+		// return to NoteBean list
+		model.addAttribute("patHistory", historyProxy.getPatientHistory(patientId));
+		model.addAttribute("currentPage", "history");
+		return "redirect:/note/list";
+	}
 
-    /**
-     * Deletes a note.
-     *
-     * @param patientId          ID of note to be deleted
-     * @param model              holder for context data to be passed from controller to the view, contains list of notes
-     * @param redirectAttributes redirection attributes, contains success or failure popup
-     * @return list of notes page
-     */
-    @GetMapping("/patHistory/{patientId}/delete/{noteId}")
-    public String deletePatient(@PathVariable("patientId") Integer patientId, @PathVariable("noteId") String noteId, Model model, RedirectAttributes redirectAttributes) {
-        PatientBean patient = getPatientIfExists(patientId, redirectAttributes);
-        if (patient == null) {
-            return "patient/list";
-        }
-        // Find NoteBean by ID
-        NoteBean note = historyProxy.getNoteById(noteId);
-        // and delete the note
-        if (note != null) {
-            historyProxy.deleteNote(noteId);
-            redirectAttributes.addFlashAttribute("success", "Note " + note.getContent() + " was successfully deleted.");
-            model.addAttribute("patient", patient);
-        } else {
-            redirectAttributes.addFlashAttribute("error", "Note with ID " + noteId + " does not exist.");
-        }
-        // return to NoteBean list
-        model.addAttribute("patHistory", historyProxy.getPatientHistory(patientId));
-        return "redirect:/note/list";
-    }
-
-    /**
-     * Checks if patient for which you want to show history or update a note for really exists. If so, the process goes on, if not, an error message is thrown.
-     *
-     * @param patientId          Note to be updated or deleted.
-     * @param redirectAttributes error message
-     * @return patient if exists, null otherwise
-     */
-    private PatientBean getPatientIfExists(Integer patientId, RedirectAttributes redirectAttributes) {
-        try {
-           return patientProxy.getPatientById(patientId);
-        } catch (PatientNotFoundException patientNotFoundException) {
-            log.error("Patient with the provided ID " + patientId + " was not found.");
-            redirectAttributes.addFlashAttribute("error", "Patient with ID " + patientId + "does not exist.");
-        }
-        return null;
-    }
+	/**
+	 * Checks if patient for which you want to show history or update a note for really exists. If so, the process goes on, if not, an error message is thrown.
+	 *
+	 * @param patientId          Note to be updated or deleted.
+	 * @param redirectAttributes error message
+	 * @return patient if exists, null otherwise
+	 */
+	private PatientBean getPatientIfExists(Integer patientId, RedirectAttributes redirectAttributes) {
+		try {
+			return patientProxy.getPatientById(patientId);
+		} catch (PatientNotFoundException patientNotFoundException) {
+			log.error("Patient with the provided ID " + patientId + " was not found.");
+			redirectAttributes.addFlashAttribute("error", "Patient with ID " + patientId + "does not exist.");
+		}
+		return null;
+	}
 }
