cf93325c4010b3cc55057c41fba83d4b34155a25
ernhollam <ernholla.marinasy@gmail.com>
2023-07-26 08:07:23 +0200

    Add documentation for PatientController

--- patients/src/main/java/com/abernathyclinic/patients/controller/PatientController.java
+++ patients/src/main/java/com/abernathyclinic/patients/controller/PatientController.java
@@ -1,12 +1,13 @@
 package com.abernathyclinic.patients.controller;
 
-
 import com.abernathyclinic.patients.exception.AlreadyExistsException;
 import com.abernathyclinic.patients.exception.InvalidFormException;
 import com.abernathyclinic.patients.exception.PatientNotFoundException;
 import com.abernathyclinic.patients.model.Patient;
 import com.abernathyclinic.patients.service.PatientService;
 import com.abernathyclinic.patients.util.InvalidFormMessageBuilder;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.Assert;
@@ -27,51 +28,59 @@ import java.util.List;
 @RestController
 @RequestMapping("/patient")
 public class PatientController {
-    @Autowired
-    PatientService patientService;
+	@Autowired
+	PatientService patientService;
 
+	@Operation(summary = "Creates new patient")
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Patient createPatient(@Valid @RequestBody Patient patient, Errors errors)
+			throws AlreadyExistsException, InvalidFormException {
+		if (errors.hasErrors()) {
+			InvalidFormMessageBuilder.buildErrorMessage(errors);
+		}
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Patient createPatient(@Valid @RequestBody Patient patient, Errors errors) throws AlreadyExistsException, InvalidFormException {
-        if (errors.hasErrors()) {
-            InvalidFormMessageBuilder.buildErrorMessage(errors);
-        }
+		return patientService.createPatient(patient);
+	}
 
-        return patientService.createPatient(patient);
-    }
+	@Operation(summary = "Gets the list of patients")
+	@GetMapping
+	public List<Patient> getPatients() {
+		return patientService.getPatients();
+	}
 
-    @GetMapping
-    public List<Patient> getPatients() {
-        return patientService.getPatients();
-    }
+	@Operation(summary = "Gets a patient by their id")
+	@GetMapping("/{id}")
+	public Patient getPatient(@Parameter(description = "id of patient to be searched") @PathVariable String id) {
+		return patientService.getPatientById(Integer.valueOf(id))
+				.orElseThrow(() -> new PatientNotFoundException("Patient with the provided ID does not exist."));
+	}
 
-    @GetMapping("/{id}")
-    public Patient getPatient(@PathVariable String id) {
-        return patientService.getPatientById(Integer.valueOf(id))
-                .orElseThrow(() -> new PatientNotFoundException("Patient with the provided ID does not exist."));
-    }
+	@Operation(summary = "Updates a patient by their id")
+	@PutMapping("/{id}")
+	public Patient updatePatient(@Parameter(description = "id of patient to be searched") @PathVariable String id,
+			@Valid @RequestBody Patient patient, Errors errors)
+			throws PatientNotFoundException, InvalidFormException {
+		if (patientService.getPatientById(Integer.valueOf(id)).isEmpty()) {
+			throw new PatientNotFoundException("Patient with the provided ID does not exist.");
+		}
+		if (errors.hasErrors()) {
+			InvalidFormMessageBuilder.buildErrorMessage(errors);
+		}
+		return patientService.updatePatient(patient);
+	}
 
-    @PutMapping("/{id}")
-    public Patient updatePatient(@PathVariable String id, @Valid @RequestBody Patient patient, Errors errors) throws PatientNotFoundException, InvalidFormException {
-        if (patientService.getPatientById(Integer.valueOf(id)).isEmpty()) {
-            throw new PatientNotFoundException("Patient with the provided ID does not exist.");
-        }
-        if (errors.hasErrors()) {
-            InvalidFormMessageBuilder.buildErrorMessage(errors);
-        }
-        return patientService.updatePatient(patient);
-    }
-
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletePatient(@PathVariable String id) throws PatientNotFoundException {
-        Assert.notNull(id, "ID must not be empty");
-        if (patientService.getPatientById(Integer.valueOf(id)).isEmpty()) {
-            throw new PatientNotFoundException("Patient with the provided ID does not exist.");
-        }
-        patientService.deletePatient(patientService.getPatientById(Integer.valueOf(id)).get());
-    }
+	@Operation(summary = "Removes a requested patient")
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletePatient(@Parameter(description = "id of patient to be deleted") @PathVariable String id)
+			throws PatientNotFoundException {
+		Assert.notNull(id, "ID must not be empty");
+		if (patientService.getPatientById(Integer.valueOf(id)).isEmpty()) {
+			throw new PatientNotFoundException("Patient with the provided ID does not exist.");
+		}
+		patientService.deletePatient(patientService.getPatientById(Integer.valueOf(id)).get());
+	}
 
 }
 
