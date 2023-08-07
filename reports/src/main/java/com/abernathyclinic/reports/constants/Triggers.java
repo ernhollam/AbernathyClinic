6189c828c121abe757e8f2fbae9172dbf73a4938
ernhollam <ernholla.marinasy@gmail.com>
2023-08-08 00:15:46 +0200

    Create list of triggers

---
+++ reports/src/main/java/com/abernathyclinic/reports/constants/Triggers.java
@@ -0,0 +1,19 @@
+package com.abernathyclinic.reports.constants;
+
+import java.util.List;
+
+public class Triggers {
+	public static final List<String> triggers = List.of(
+			"Hémoglobine A1C",
+			"Microalbumine",
+			"Taille",
+			"Poids",
+			"Fumeur",
+			"Anormal",
+			"Cholestérol",
+			"Vertige",
+			"Rechute",
+			"Réaction",
+			"Anticorps"
+	);
+}
