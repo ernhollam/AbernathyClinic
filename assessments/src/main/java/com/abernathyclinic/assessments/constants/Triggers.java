f76946fc7b544d25640a1bf7f619d711fb99b866
ernhollam <ernholla.marinasy@gmail.com>
2023-08-08 03:50:16 +0200

    Refactor constants for each risk level trigger count

--- assessments/src/main/java/com/abernathyclinic/assessments/constants/Triggers.java
+++ assessments/src/main/java/com/abernathyclinic/assessments/constants/Triggers.java
@@ -34,11 +34,4 @@ public class Triggers {
 			"Anticorps"
 	);
 
-	public static final int BORDERLINE_OVER_AGE_LIMIT        = 2;
-	public static final int IN_DANGER_FEMALE_TRIGGER_COUNT   = 4;
-	public static final int IN_DANGER_MALE_TRIGGER_COUNT     = 3;
-	public static final int IN_DANGER_OVER_AGE_LIMIT         = 6;
-	public static final int EARLY_ONSET_FEMALE_TRIGGER_COUNT = 7;
-	public static final int EARLY_ONSET_MALE_TRIGGER_COUNT   = 5;
-	public static final int EARLY_ONSET_OVER_AGE_LIMIT       = 8;
 }
---
+++ assessments/src/test/java/com/abernathyclinic/assessments/service/PatientProfileServiceTest.java
@@ -0,0 +1,24 @@
+package com.abernathyclinic.assessments.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+// TODO
+class PatientProfileServiceTest {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@Test
+	void getAge() {
+	}
+
+	@Test
+	void isOverAgeLimit() {
+	}
+
+	@Test
+	void isFemale() {
+	}
+}
