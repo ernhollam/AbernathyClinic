284f0c89e0651323cb3fa4d79d4622149af8e8c0
ernhollam <ernholla.marinasy@gmail.com>
2023-08-14 14:29:32 +0200

    Use unified test cases

--- patients/src/main/resources/database/data.sql
+++ patients/src/main/resources/database/data.sql
@@ -1,12 +1,6 @@
 INSERT INTO patients (family, given, dob, sex, address, phone) VALUES
-('Ferguson', 'Lucas', '1968-06-22', 'M', '2 Warren Street', '387-866-1399'),
-('Rees', 'Pippa', '1952-09-27', 'F', '745 West Valley Farms Drive', '628-423-0993'),
-('Arnold', 'Edward', '1952-11-11', 'M', '599 East Garden Ave', '123-727-2779'),
-('Sharp', 'Anthony', '1946-11-26', 'M', '894 Hall Street', '451-761-8383'),
-('Ince', 'Wendy', '1958-06-29', 'F', '4 Southampton Road', '802-911-9975'),
-('Ross', 'Tracey', '1949-12-07', 'F', '40 Sulphur Springs Dr', '131-396-5049'),
-('Wilson', 'Claire', '1966-12-31', 'F', '12 Cobblestone St', '300-452-1091'),
-('Buckland', 'Max', '1945-06-24', 'M', '193 Vale St', '833-534-0864'),
-('Clark', 'Natalie', '1964-06-18', 'F', '12 Beechwood Road', '241-467-9197'),
-('Bailey', 'Piers', '1959-06-28', 'M', '1202 Bumble Dr', '747-815-0557')
+('TestNone', 'Test', '1966-12-31', 'F', '1 Brookside St', '100-222-3333'),
+('TestBorderline', 'Test', '1945-06-24', 'M', '2 High St', '200-333-4444'),
+('TestInDanger', 'Test', '2004-06-18', 'M', '3 Club Road', '300-444-5555'),
+('TestEarlyOnset', 'Test', '2002-06-28', 'F', '4 Valley Dr', '400-555-6666')
 ;
