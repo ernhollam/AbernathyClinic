11b2a2fa409a2679a6cd445f1d2551233c9be9bd
ernhollam <ernholla.marinasy@gmail.com>
2023-08-14 14:54:42 +0200

    Implement countTriggers test

--- assessments/src/test/java/com/abernathyclinic/assessments/service/RiskAssessmentServiceTest.java
+++ assessments/src/test/java/com/abernathyclinic/assessments/service/RiskAssessmentServiceTest.java
@@ -1,10 +1,86 @@
 package com.abernathyclinic.assessments.service;
 
+import com.abernathyclinic.assessments.bean.NoteBean;
+import com.abernathyclinic.assessments.bean.PatientBean;
+import com.abernathyclinic.assessments.proxy.HistoryProxy;
+import com.abernathyclinic.assessments.proxy.PatientProxy;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-//TODO
+import java.time.Clock;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class RiskAssessmentServiceTest {
+	@Autowired
+	RiskAssessmentService riskAssessmentService;
+	// set now() to the 1st August 2023
+	public final static LocalDate             LOCAL_DATE_NOW = LocalDate.of(2023, 8, 1);
+	@MockBean
+	private             Clock                 clock;
+	@MockBean
+	private             PatientProxy          patientProxy;
+	@MockBean
+	private             HistoryProxy          historyProxy;
+	@MockBean
+	private             PatientProfileService patientProfileService;
+
+	private PatientBean testNone;
+	private PatientBean testBorderline;
+	private PatientBean testInDanger;
+	private PatientBean testEarlyOnset;
+
+	private final List<NoteBean> testNonesNotes       = new ArrayList<>();
+	private final List<NoteBean> testBorderlinesNotes = new ArrayList<>();
+	private final List<NoteBean> testInDangersNotes   = new ArrayList<>();
+	private final List<NoteBean> testEarlyOnsetsNotes = new ArrayList<>();
+
+	@BeforeAll
+	void init() {
+		testNone       = new PatientBean(1, "TestNone", "Test", LocalDate.of(1966, 12, 31), "F", "1 Brookside St",
+				"00-222-3333");
+		testBorderline = new PatientBean(2, "TestBorderline", "Test", LocalDate.of(1945, 6, 24), "M", "2 High St",
+				"200-333-444");
+		testInDanger   = new PatientBean(3, "TestInDanger", "Test", LocalDate.of(2004, 6, 18), "M", "3Club Road",
+				"300-444-5555");
+		testEarlyOnset = new PatientBean(4, "TestEarlyOnset", "Test", LocalDate.of(2002, 6, 28), "F", "4 Valley Dr",
+				"400-555-6666");
+
+		testNonesNotes.add(new NoteBean(1,
+				"Le patient déclare qu'il 'se sent très bien' Poids égal ou inférieur au poids recommandé"));
+
+		testBorderlinesNotes.addAll(List.of(
+				new NoteBean(2, "\n" +
+						"Le patient déclare qu'il ressent beaucoup de stress au travail. Il se plaint également que son audition est anormale dernièrement"),
+				new NoteBean(2,
+						"Le patient déclare avoir fait une réaction aux médicaments au cours des 3 derniers mois Il remarque également que son audition continue d'être anormale")
+		));
+
+		testInDangersNotes.addAll(List.of(
+				new NoteBean(3, "Le patient déclare qu'il fume depuis peu"),
+				new NoteBean(3,
+						"Le patient déclare qu'il est fumeur et qu'il a cessé de fumer l'année dernière Il se plaint également de crises d’apnée respiratoire anormales Tests de laboratoire indiquant un taux de cholestérol LDL élevé")
+		));
+		testEarlyOnsetsNotes.addAll(List.of(
+				new NoteBean(4,
+						"Le patient déclare qu'il lui est devenu difficile de monter les escaliers Il se plaint également d’être essoufflé Tests de laboratoire indiquant que les anticorps sont élevés Réaction aux médicaments"),
+				new NoteBean(4, "Le patient déclare qu'il a mal au dos lorsqu'il reste assis pendant longtemps"),
+				new NoteBean(4,
+						"Le patient déclare avoir commencé à fumer depuis peu Hémoglobine A1C supérieure au niveau recommandé"),
+				new NoteBean(4, "Taille, Poids, Cholestérol, Vertige et Réaction")
+		));
+
+	}
 
 	@BeforeEach
 	void setUp() {
@@ -16,5 +92,9 @@ class RiskAssessmentServiceTest {
 
 	@Test
 	void countTriggers() {
+		assertEquals(1, riskAssessmentService.countTriggers(testNonesNotes));
+		assertEquals(2, riskAssessmentService.countTriggers(testBorderlinesNotes));
+		assertEquals(3, riskAssessmentService.countTriggers(testInDangersNotes));
+		assertEquals(7, riskAssessmentService.countTriggers(testEarlyOnsetsNotes));
 	}
 }
