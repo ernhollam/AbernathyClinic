dfebf8ec8fd0faf104fbf36ec1e4011936130736
ernhollam <ernholla.marinasy@gmail.com>
2023-08-26 04:25:50 +0200

    Use a unique file to create SQL database schema definition and population

--- patients/src/main/resources/database/initDb.sql
+++ patients/src/main/resources/database/initDb.sql
@@ -1,3 +1,18 @@
+DROP DATABASE IF EXISTS abernathy_clinic ;
+
+CREATE DATABASE abernathy_clinic;
+USE abernathy_clinic;
+
+CREATE TABLE patients (
+    patient_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    family VARCHAR(50) NOT NULL,
+    given VARCHAR(50) NOT NULL,
+    dob DATE NOT NULL,
+    sex VARCHAR(1),
+    address VARCHAR(100),
+    phone VARCHAR(50)
+);
+
 INSERT INTO patients (family, given, dob, sex, address, phone) VALUES
 ('TestNone', 'Test', '1966-12-31', 'F', '1 Brookside St', '100-222-3333'),
 ('TestBorderline', 'Test', '1945-06-24', 'M', '2 High St', '200-333-4444'),
